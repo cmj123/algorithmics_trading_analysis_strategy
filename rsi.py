c664bba426e23ddfeecd388b461ca9c912f3ac83
Esuabom Dijemeni <e.dijemeni@gmail.com>
2021-11-04 17:48:21 +0000

    Completed RSI zone of action

--- rsi.py
+++ rsi.py
@@ -17,26 +17,26 @@ warnings.filterwarnings("ignore")
 #                 ['#669'])
 
 # import the data
-f = yf.download(["GOOG"]) # ,"APPL"
+f = yf.download(["PLUG"]) # ,"APPL"
 # Compute the rsi 
 f["rsi"] = ta.momentum.RSIIndicator(f["Adj Close"], window=14).rsi()
-print(f.tail())
-# Plot the RSI with the overbuy and oversell threshold
-plt.figure(figsize=(15, 8))
 
-# View the RSI 
+## Creating RSI zone of action
+plt.figure(figsize=(15,8))
+
+# View RSI
 plt.plot(f["rsi"].loc["2021"])
 
-# View horizontal line for the overbuy threshold (RSI = 70)
-plt.axhline(70, color="#57CE95")
+# View buy zone of action
+plt.fill_between(f["rsi"].loc["2021"].index, 55,70, color="#57CE95", alpha=0.5)
 
-# View horizontal line for the oversell thresold (RSI = 30)
-plt.axhline(30, color="#CE5757")
+# View sell zone of action
+plt.fill_between(f["rsi"].loc["2021"].index, 45,30, color="#CE5757", alpha=0.5)
 
 # Put a title 
-plt.title("RSI with thresholds")
+plt.title("RSI with zone on long buy and short sell")
 
 # Put a legend
-plt.legend(["RSI", "Overbuy", "Oversell"])
+plt.legend(["RSI", "Long buy signal", "Short sell zone"])
 
 plt.show()
